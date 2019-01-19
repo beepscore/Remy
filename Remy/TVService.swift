dce799ac67680e7e1289cdea65ec8c0c72bb2a72
beepscore <support@beepscore.com>
2019-01-19 12:28:56 -0800

    Add volumeDecrease

--- Remy/TVService.swift
+++ Remy/TVService.swift
@@ -65,4 +65,46 @@ class TVService {
         task.resume()
     }
 
+    /// make a web request to a service to decrease volume
+    func volumeDecrease() {
+
+        // https://stackoverflow.com/questions/26364914/http-request-in-swift-with-post-method#26365148
+        let urlString = TVService.urlBasePortApiVersion() + "/tv/volume_decrease/"
+        let url = URL(string: urlString)!
+        var request = URLRequest(url: url)
+
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        request.httpMethod = "POST"
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+
+            guard let data = data, error == nil else {
+                // fundamental networking error
+                print("error=\(String(describing: error))")
+                return
+            }
+
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                // http error
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(String(describing: response))")
+                return
+            }
+
+            let decoder = JSONDecoder()
+            do {
+                let tvResponse = try decoder.decode(TVResponse.self, from: data)
+                print("tvResponse", tvResponse)
+
+                if tvResponse.status == "SUCCESS" {
+                    // TODO: consider show toast similar to Android toast
+                }
+            } catch {
+                print("error trying to convert data to JSON")
+                print(error)
+            }
+        }
+        task.resume()
+    }
+
 }
