7b984c42b9ee675b27af3a59bce5dd66d1c17811
beepscore <support@beepscore.com>
2019-09-22 12:39:10 -0700

    list another example error

--- Remy/RemoteControlVC.swift
+++ Remy/RemoteControlVC.swift
@@ -101,7 +101,9 @@ class RemoteControlVC: UIViewController {
             case .success(let tvResponse):
                 self.statusLabel.text = tvResponse.message
             case .failure(let error):
-                // e.g. "Could not connect to the server"
+                // e.g.
+                // "unsupported URL"
+                // "Could not connect to the server"
                 // "404 Not Found"
                 self.statusLabel.text = error.localizedDescription
             }
