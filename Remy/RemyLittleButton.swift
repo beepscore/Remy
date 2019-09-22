5d4014d82da73bfd5a3cea4a8566e824f6eeeb97
beepscore <support@beepscore.com>
2019-09-22 11:38:54 -0700

    Add RemyLittleButton

---
+++ Remy/RemyLittleButton.swift
@@ -0,0 +1,29 @@
+//
+//  RemyLittleButton.swift
+//  Remy
+//
+//  Created by Steve Baker on 9/22/19.
+//  Copyright © 2019 Beepscore LLC. All rights reserved.
+//
+
+import UIKit
+
+class RemyLittleButton: UIButton {
+
+    /// initializes programmatically
+    override init (frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    /// initialize from storyboard or nib
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        configure()
+    }
+
+    func configure() {
+        layer.cornerRadius = CGFloat(8.0)
+    }
+
+}
