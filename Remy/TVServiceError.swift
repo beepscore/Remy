0bf1d5a0852454bf169d7a2488da71fd40257104
beepscore <support@beepscore.com>
2019-10-12 06:48:35 -0700

    In TVSericeError add comments

--- Remy/TVServiceError.swift
+++ Remy/TVServiceError.swift
@@ -10,10 +10,13 @@ import Foundation
 
 enum TVServiceError: LocalizedError {
     case dataNil
-    // don't implement case decodingError, use Swift DecodingError instead
+
+    // case decodingError Don't implement, use Swift DecodingError instead
+
     // https://stackoverflow.com/questions/39176196/how-to-provide-a-localized-description-with-an-error-type-in-swift
     // httpError has associated values
     case httpError(status: Int, message: String)
+
     case responseNil
     case urlNil
 }
@@ -22,14 +25,22 @@ extension TVServiceError {
 
     public var errorDescription: String? {
         switch self {
+            
         case .dataNil:
             return NSLocalizedString("Data nil", comment: "Response nil")
+
         case let .httpError(status, message):
-            // e.g. "404 Not Found"
+            // use the associated values status and message
+            // e.g. status 404 and message "Not Found" yields "404 Not Found"
             return "\(status) \(message)"
+
         case .responseNil:
             return NSLocalizedString("Response nil", comment: "Response nil")
+            
         default:
+            // any other LocalizedError or other TVServiceError
+            // e.g. Swift DecodingError with English localizedDescription "The data couldn't be read because it is missing"
+            // e.g. TVServiceError.urlNil with original LocalizedError localizedDescription
             return self.localizedDescription
         }
     }
