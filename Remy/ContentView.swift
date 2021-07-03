c67c041cfbd9008ad3a64d2095658616ebbc5d40
beepscore <support@beepscore.com>
2021-07-03 09:28:55 -0700

    Start to convert from UIKit to SwiftUI.
    In another directory I made a new SwiftUI project named Remy. Then in existing Remy made a git branch "swiftui" and copied files from new Remy into existing Remy.
    This overwrote files like pbxproj, Info.plist, Assets
    TODO: bring other needed swift files into pbxproj. Make SwiftUI Views similar to storyboards and delete unneeded storyboards and view controllers.

---
+++ Remy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Remy
+//
+//  Created by Steve Baker on 7/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Remy/RemyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemyApp.swift
+//  Remy
+//
+//  Created by Steve Baker on 7/3/21.
+//
+
+import SwiftUI
+
+@main
+struct RemyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
