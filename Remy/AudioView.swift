5d5b64787b3c6318e0d330db72a589eecb626683
beepscore <support@beepscore.com>
2021-07-03 21:22:40 -0700

    In AudioView add audioLevelSliderAccentColor and use it

--- Remy/AudioView.swift
+++ Remy/AudioView.swift
@@ -13,11 +13,21 @@ struct AudioView: View {
     // levelMaxDb == 0 and levelMinDb is < 0, so use levelMinDb/2
     @State var audioLimit = AudioMonitor.levelMinDb / 2
 
+    static func audioLevelSliderAccentColor(audioLevel: Float, audioLimit: Float) -> Color {
+        if audioLevel > audioLimit {
+            return .red
+        } else {
+            return .green
+        }
+    }
+
     var body: some View {
         VStack {
             VStack() {
                 Text("audio level dB: \(Int(audioMonitor.level))")
                 Slider(value: $audioMonitor.level, in: AudioMonitor.levelMinDb...AudioMonitor.levelMaxDb)
+                    .accentColor(AudioView.audioLevelSliderAccentColor(audioLevel: audioMonitor.level,
+                                                                       audioLimit: audioLimit))
                     // allowsHitTesting false disables user interaction
                     .allowsHitTesting(/*@START_MENU_TOKEN@*/false/*@END_MENU_TOKEN@*/)
             }
@@ -25,6 +35,7 @@ struct AudioView: View {
             VStack() {
                 Text("limit dB: \(Int(audioLimit))")
                 Slider(value: $audioLimit, in: AudioMonitor.levelMinDb...AudioMonitor.levelMaxDb)
+                    .accentColor(.gray)
             }
         }
     }
