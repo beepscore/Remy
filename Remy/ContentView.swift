5d341b9f26937d8cfc1c00c4f82f840ab83d1392
beepscore <support@beepscore.com>
2021-07-03 20:24:06 -0700

    Add audioLimit and slider.

--- Remy/ContentView.swift
+++ Remy/ContentView.swift
@@ -9,10 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
 
+    @EnvironmentObject var audioMonitor: AudioMonitor
+
     @ObservedObject var tvService = TVService(timeoutSeconds: 10.0)
+
+    @State var audioLimit: Float = 0.0
 
     var body: some View {
         VStack {
+
+            HStack() {
+                Text("limit: \(audioLimit)")
+                Slider(value: $audioLimit, in: AudioMonitor.levelMinDb...AudioMonitor.levelMaxDb)
+            }
 
             Text(tvService.statusText)
                 .font(.title2)
